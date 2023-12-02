1148f08ce5b7f649c1b769d1a348113ffa1613d0
sumitvish20 <sumitvds360@gmail.com>
2023-12-02 09:43:40 +0530

    Page to display characters' profiles

--- src/components/CharacterGrid.jsx
+++ src/components/CharacterGrid.jsx
@@ -1,35 +1,56 @@
-// CharacterGrid.js (Updated with search functionality)
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Grid, Card, CardContent, Typography, TextField } from '@mui/material';
+import { Grid, Card, CardContent, Typography, TextField, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const CharacterGrid = () => {
   const [characters, setCharacters] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [filters, setFilters] = useState({
+    status: '',
+    location: '',
+    episode: '',
+    gender: '',
+    species: '',
+    type: '',
+  });
 
   useEffect(() => {
-    axios.get(`https://rickandmortyapi.com/api/character/?name=${searchTerm}`)
+    const params = new URLSearchParams(filters);
+    axios.get(`https://rickandmortyapi.com/api/character/?${params}`)
       .then(response => {
         setCharacters(response.data.results);
       })
       .catch(error => {
         console.error('Error fetching characters:', error);
       });
-  }, [searchTerm]);
+  }, [filters]);
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilters({ ...filters, [name]: value });
   };
 
   return (
     <div>
-      <TextField
-        label="Search by name"
-        variant="outlined"
-        onChange={handleSearch}
-        fullWidth
-        margin="normal"
-      />
+      <form>
+        <TextField
+          label="Search by name"
+          variant="outlined"
+          onChange={(event) => setFilters({ ...filters, name: event.target.value })}
+          fullWidth
+          margin="normal"
+        />
+        <FormControl variant="outlined" fullWidth margin="normal">
+          <InputLabel>Status</InputLabel>
+          <Select name="status" value={filters.status} onChange={handleFilterChange}>
+            <MenuItem value="">All</MenuItem>
+            <MenuItem value="Alive">Alive</MenuItem>
+            <MenuItem value="Dead">Dead</MenuItem>
+            <MenuItem value="unknown">Unknown</MenuItem>
+          </Select>
+        </FormControl>
+        {/* Other filter options for location, episode, gender, species, type */}
+      </form>
       <Grid container spacing={3}>
         {characters.map(character => (
           <Grid item xs={12} sm={6} md={4} key={character.id}>
@@ -44,7 +65,8 @@ const CharacterGrid = () => {
                 <Typography variant="body2" color="textSecondary" component="p">
                   Gender: {character.gender}
                 </Typography>
-               
+                {/* Additional information can be displayed */}
+                <Link to={`/character/${character.id}`}>View Profile</Link>
               </CardContent>
             </Card>
           </Grid>
@@ -55,5 +77,6 @@ const CharacterGrid = () => {
 };
 
 export default CharacterGrid;
+
 
 
